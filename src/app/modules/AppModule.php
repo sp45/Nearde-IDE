47d31538d2d0528408bd170b7e7e90dfd707c3ac
Максим Тарасов <yousan4ik@gmail.com>
2018-03-04 19:07:25 +0300

    Реализовоно открытие проекта если его файл передать при запуске IDE

--- src/app/modules/AppModule.php
+++ src/app/modules/AppModule.php
@@ -1,6 +1,8 @@
 <?php
 namespace app\modules;
 
+use utils\Project;
+use app\modules\MainModule;
 use facade\Json;
 use std, gui, framework, app;
 
@@ -13,7 +15,16 @@ class AppModule extends AbstractModule
      */
     function doAction(ScriptEvent $e = null)
     {    
-        
+        if (fs::ext($GLOBALS['argv'][1]) == "nrd")
+        {
+            $project = new \utils\Project;
+            if (!$project->Open(fs::parent($GLOBALS['argv'][1]), fs::nameNoExt($GLOBALS['argv'][1])))
+            {
+                app()->showForm("MainForm");
+            }
+        } else {
+            app()->showForm("MainForm");
+        }
     }
 
 }
