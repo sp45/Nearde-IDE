60cad745c04c3e02429608873dc431f86f403020
Максим Тарасов <yousan4ik@gmail.com>
2018-04-22 01:01:55 +0400

    fix

--- src/nd/Plugin.php
+++ src/nd/Plugin.php
@@ -6,9 +6,4 @@ abstract class Plugin
     abstract public function getName();
     abstract public function getIcon();
     abstract public function getDscription();
-    
-    public function __construct()
-    {
-        call_user_func("onIDEStarting", null);
-    }
 }
